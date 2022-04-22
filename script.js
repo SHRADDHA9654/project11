a8f3c874c937e69dcaa1509a6d2112d9cb552f29
SHRADDHA9654 <67624451+SHRADDHA9654@users.noreply.github.com>
2022-04-21 22:24:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+var img=document.querySelector(".img12");
+img.addEventListener("mouseenter" , car);
+img.addEventListener("mouseleave" , rac);
+
+
+
+function car() {
+   
+   img.style.transform="rotate(720deg)" 
+   img.style.transition="4s ease"
+}
+
+function rac(){
+    img.style.transform="rotate(-720deg)" 
+    img.style.transition="1s ease"
+}
+
+var img1=document.querySelector(".img13");
+img1.addEventListener("mouseenter" , re);
+img1.addEventListener("mouseleave" , er);
+
+
+
+function re() {
+   
+   img1.style.transform="rotate(720deg)" 
+   img1.style.transition="4s ease"
+}
+
+function er(){
+    img1.style.transform="rotate(-720deg)" 
+    img1.style.transition="1s ease"
+}
+
+
+var img2=document.querySelector(".img14");
+img2.addEventListener("mouseenter" , lov);
+img2.addEventListener("mouseleave" , vol);
+
+
+
+function lov() {
+   
+   img2.style.transform="rotate(720deg)" 
+   img2.style.transition="4s ease"
+}
+
+function vol(){
+    img2.style.transform="rotate(-720deg)" 
+    img2.style.transition="1s ease"
+}
+
+var img3=document.querySelector(".img15");
+img3.addEventListener("mouseenter" , map);
+img3.addEventListener("mouseleave" , pam);
+
+
+
+function map() {
+   
+   img3.style.transform="rotate(720deg)" 
+   img3.style.transition="4s ease"
+}
+
+function pam(){
+    img3.style.transform="rotate(-720deg)" 
+    img3.style.transition="1s ease"
+}
+
+var img4=document.querySelector(".img16");
+img4.addEventListener("mouseenter" , cat);
+img4.addEventListener("mouseleave" , tac);
+
+
+
+function cat() {
+   
+   img4.style.transform="rotate(720deg)" 
+   img4.style.transition="4s ease"
+}
+
+function tac(){
+    img4.style.transform="rotate(-720deg)" 
+    img4.style.transition="1s ease"
+}
+
+var img5=document.querySelector(".img17");
+img5.addEventListener("mouseenter" , bo);
+img5.addEventListener("mouseleave" , ob);
+
+
+
+function bo() {
+   
+   img5.style.transform="rotate(720deg)" 
+   img5.style.transition="4s ease"
+}
+
+function ob (){
+    img5.style.transform="rotate(-720deg)" 
+    img5.style.transition="1s ease"
+}
